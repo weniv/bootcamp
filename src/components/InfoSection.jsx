9caa998612fac6d330100e791c26720f520f7dcd
dreamfulbud <dreamfulbud@naver.com>
2023-11-24 16:23:52 +0900

    💄 Style: 모바일 CSS 수정

--- src/components/InfoSection.jsx
+++ src/components/InfoSection.jsx
@@ -1,7 +1,7 @@
 import classNames from 'classnames';
 import styles from './InfoSection.module.scss';
 
-export default function InfoSection({ img, alt, teacher, children }) {
+export default function InfoSection({ img, alt, children }) {
   return (
     <div className={classNames('info-box', styles['info-box'])}>
       <img src={img} alt={alt ? alt : ''} />
