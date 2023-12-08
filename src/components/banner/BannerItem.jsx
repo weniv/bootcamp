645de54723457ed353c22e6622f4e6b4292a5345
dreamfulbud <dreamfulbud@naver.com>
2023-12-08 10:43:55 +0900

    📝 Eidt: 링크 버튼 -> 배너 클릭 형태로 수정

--- src/components/banner/BannerItem.jsx
+++ src/components/banner/BannerItem.jsx
@@ -2,33 +2,48 @@ import styles from './Banner.module.scss';
 
 import Btn from '../button/Btn';
 
+const Content = ({
+  categoryColor,
+  thumbnail,
+  category,
+  title,
+  description,
+}) => {
+  return (
+    <div className="max-width">
+      <div className={styles.content}>
+        <p className={styles.category} style={{ color: categoryColor }}>
+          {category}
+        </p>
+        <h3 className={styles.title}>{title}</h3>
+
+        <p className={styles.description}>
+          {description[0]}
+          <br />
+          <strong>{description[1]}</strong>
+        </p>
+      </div>
+
+      {thumbnail && <img className={styles.thumbnail} src={thumbnail} alt="" />}
+    </div>
+  );
+};
 export default function BannerItem({ data }) {
-  const { bgcolor, thumbnail, category, title, description, link } = data;
+  const { bgColor, textColor, link, ...rest } = data;
+
+  const isBlank = link ? link.includes('http') : null;
 
   return (
-    <article className={styles.bannerItem} style={{ backgroundColor: bgcolor }}>
-      <div className="max-width">
-        <div className={styles.content}>
-          <p className={styles.category}>{category}</p>
-          <h3 className={styles.title}>{title}</h3>
-
-          <p className={styles.description}>
-            {description[0]}
-            <br />
-            <strong>{description[1]}</strong>
-          </p>
-
-          {link && (
-            <Btn href={link} solid="true">
-              바로가기
-            </Btn>
-          )}
-        </div>
-
-        {thumbnail && (
-          <img className={styles.thumbnail} src={thumbnail} alt="" />
-        )}
-      </div>
+    <article
+      className={styles.bannerItem}
+      style={{ backgroundColor: bgColor, color: textColor }}>
+      {link && link ? (
+        <a href={link} target={isBlank ? '_blank' : ''}>
+          <Content {...rest} />
+        </a>
+      ) : (
+        <Content {...rest} />
+      )}
     </article>
   );
 }
