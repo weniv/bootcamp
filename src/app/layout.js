99ff7d002f49d9191efb052f39a376f77240debd
dreamfulbud <dreamfulbud@naver.com>
2023-12-29 14:57:08 +0900

    🔍 SEO: SEO 작업

--- src/app/layout.js
+++ src/app/layout.js
@@ -6,9 +6,8 @@ import '@/styles/globals.scss';
 export const metadata = {
   title: '위니브 부트캠프',
   description:
-    '100% 온라인 과정으로 진행되는 단기 부트캠프에서는 위니브의 실무 경험을 바탕으로 만들어진 현실 도달 가능한 커리큘럼과 프로젝트, 멘토링을 제공합니다.',
+    '위니브의 실무 경험을 바탕으로 만들어진 현실 도달 가능한 커리큘럼과 프로젝트, 멘토링을 제공합니다.',
   metadataBase: new URL('https://bootcamp.weniv.co.kr'),
-
   icons: {
     icon: '/favicon.ico',
     shortcut: '/favicon.ico',
@@ -32,11 +31,33 @@ export const metadata = {
   },
 };
 
-const GA_ID = process.env.GA_ID;
+const GA_ID = process.env.NEXT_PUBLIC_GA_ID;
 
 export default function RootLayout({ children }) {
   return (
     <html lang="ko-KR">
+      <head>
+        <script
+          async
+          src={`https://www.googletagmanager.com/gtag/js?id=${GA_ID}`}
+        />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', '${GA_ID}', {
+            page_path: window.location.pathname,
+          });
+        `,
+          }}
+        />
+        <meta
+          name="naver-site-verification"
+          content="c51f4cbf1d086158b6a1751c56d9483d32efb231"
+        />
+      </head>
       <body className="layout-flex">
         {children}
         <BtnTop />
